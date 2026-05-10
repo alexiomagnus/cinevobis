4e29e5c435674371efc402d3cf4e6c1fcaf40883
Alessio <alessio.gualtieri187@gmail.com>
2026-05-10 12:28:35 +0200

    refactor: implement environment variable loading for database connection configuration

--- config/connection.php
+++ config/connection.php
@@ -8,19 +8,26 @@
  *
  * @note Interagisce con il database MariaDB: `cinevobis`.
  */
-$host = '127.0.0.1';
-$dbname = 'cinevobis';
-$user = 'root';
-$password = 'root';
+require_once(__DIR__ . '/../vendor/autoload.php');
+
+use Dotenv\Dotenv;
+
+// Inizializza Dotenv puntando alla cartella del progetto
+$dotenv = Dotenv::createImmutable(__DIR__ . "/../");
+$dotenv->load();
+
+$host = $_ENV['DB_HOST'];
+$dbname = $_ENV['DB_NAME'];
+$user = $_ENV['DB_USER'];
+$password = $_ENV['DB_PASS'];
 
 try {
     $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 } catch (PDOException $e) {
-    // Scrivere l'errore dettagliato nel log (grazie al config.php)
     error_log("Errore critico DB: " . $e->getMessage());
     
     // Mostrare all'utente un messaggio generico
-    die("Spiacenti, il servizio è momentaneamente non disponibile.");
+    die("Spiacenti, il servizio è momentaneamente non disponibile");
 }
