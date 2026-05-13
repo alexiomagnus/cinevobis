e7a5d294b84718fcc1fa22d1da7f842ef7d07702
Alessio <alessio.gualtieri187@gmail.com>
2026-05-13 22:23:46 +0200

    feat: update user profile page title to include user's name

--- pages/public/users_profiles.php
+++ pages/public/users_profiles.php
@@ -80,7 +80,7 @@ if (!empty($ids)) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Profilo - Cinevobis</title>
+    <title>Profilo di <?php echo htmlspecialchars($userData['nome']) .  " " . htmlspecialchars($userData['cognome']); ?> - Cinevobis</title>
     <link rel="stylesheet" href="/node_modules/bootstrap/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <link rel="stylesheet" href="/assets/css/style.css">
